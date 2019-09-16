8406992e05d286dbc8e4d0b4a8e2920a7f5fd813
svin <svinerus@gmail.com>
2019-09-17 00:56:23 +0300

    add midtask3, rename

--- 3.1/java/labs/lab1/src/svinerus/Main.java
+++ 3.1/java/labs/lab1/src/svinerus/Main.java
@@ -3,6 +3,6 @@ package svinerus;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Task2.makePyramid(9));
+        System.out.println(Mid.Task3(9));
     }
 }
