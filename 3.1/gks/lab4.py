c79cb7c639683b46f4d2e2c10496905d5e6e19e8
svin <svinerus@gmail.com>
2019-11-18 12:50:01 +0200

    доделал

--- 3.1/gks/lab4.py
+++ 3.1/gks/lab4.py
@@ -4,25 +4,19 @@ import lab3
 def main():
     lab3_output = lab3.main()
 
-    sorted_modules = sort_modules(lab3_output)
-    # optimized_modules = optimize_modules(sorted_modules)
+    optimized_modules = optimize_modules(lab3_output)
 
     if __name__ == "__main__":
         lab3.print_modules_by_group(lab3_output)
-        print(*sorted_modules, sep='\n')
         print_optimized_modules(optimized_modules)
 
 
-def sort_modules(modules_by_group):
+def optimize_modules(modules_by_group):
     modules = [list(item) for sublist in modules_by_group for item in sublist]
     modules = sorted(modules, key=lambda i: len(i))
-    return modules
-
-
-def optimize_modules(modules):
 
     def exist(operation, start_index):
-        for i in modules[start_index:]:
+        for i in modules[start_index+1:]:
             if operation in i:
                 return True
 
@@ -38,9 +32,6 @@ def print_optimized_modules(optimized_modules):
     print("\n\nУточненные модули: ")
     for i in optimized_modules:
         print(', '.join(i))
-
-
-
 
 
 if __name__ == "__main__":
