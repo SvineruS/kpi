b6c4081284a7221e03a4f137ad84076f84560f2b
svin <svinerus@gmail.com>
2019-12-04 01:34:56 +0200

    java lab5 хз чому этого не было в гите

--- 3.1/java/labs/lab5/src/svinerus/Main.java
+++ 3.1/java/labs/lab5/src/svinerus/Main.java
@@ -1,10 +1,21 @@
 package svinerus;
 
 import java.io.IOException;
-import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        try {
+            System.out.println("html tags in google.com: ");
+            Part4 tags = new Part4("https://google.com");
+            System.out.println("sort by count: " + tags.sortByCount());
+            System.out.println("sort by tag: " + tags.sortByTag());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
