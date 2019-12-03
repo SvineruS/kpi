fc39580d9e94fb4ab5e0704d6df35bf6e89bf2e2
svin <svinerus@gmail.com>
2019-12-04 01:30:00 +0200

    блядь не в ту папку лабу закинул))

--- 3.1/java/labs/lab7/src/svinerus/Tests.java
+++ 3.1/java/labs/lab7/src/svinerus/Tests.java
@@ -10,6 +10,5 @@ class Tests {
     @org.junit.jupiter.api.Test
     void task1() {
         assertArrayEquals(Task1(new String[]{"1", "12", "123", "1234"}), new String[]{"123", "1234"});
-        assertArrayEquals(Task1(new String[]{"1", "1", "1"}), new String[]{"1", "1", "1"});
     }
 }
