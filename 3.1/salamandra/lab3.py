48f2c9833627ab536f0106aa727cdccfba2c1b01
svin <svinerus@gmail.com>
2019-12-12 00:39:59 +0200

    lab3 again

--- 3.1/salamandra/lab3.py
+++ 3.1/salamandra/lab3.py
@@ -1,8 +1,8 @@
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
+import tkinter
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
 
-matplotlib.use("TkAgg")
+import numpy as np
 
 
 def f(x):
@@ -12,5 +12,20 @@ def f(x):
 x = np.linspace(-5, 5, 100)
 y = f(x)
 
-plt.plot(x, y)
-plt.show()
+
+root = tkinter.Tk()
+root.wm_title("Lab3")
+
+fig = Figure()
+fig.add_subplot().plot(x, y)
+
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+toolbar = NavigationToolbar2Tk(canvas, root)
+canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+button = tkinter.Button(master=toolbar, text="Пососать минет", command=lambda: root.quit())
+button.pack(side=tkinter.BOTTOM)
+
+tkinter.mainloop()
